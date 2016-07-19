e8eb1f0920a30623c18a0efdf12f6743e0558cf9
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-07-19 16:52:38 +0800

    Add more notes about accumulator

--- spark-accumulator.scala
+++ spark-accumulator.scala
@@ -1,7 +1,18 @@
+// For accumulators used in actions, Spark applies each task’s update to each accumulator only once. 
+// For accumulators used in RDD transformations instead of actions, this guarantee does not exist for speculative execution
+// Within transformations, accumulators should, conse‐ quently, be used only for debugging purposes.
+//  commutative if a op b = b op a 
+//  associative if (a op b) op c = a op (b op c) 
+
 val file = sc.textFile("file:///Users/Zhenglai/git/spark/README.md")
+
+// init the accumulator as Accumulator[Int] with 0 init value
 val blankLines = sc.accumulator(0)
+
 val lines = file flatMap { line => {
   if (line == "") {
+    // add to accumulator, counting blanklines (errors, debugging events... counting)
+    // worker code in spark closures, accumulator is write-only here
     blankLines += 1
   } 
     line.split(" ")
@@ -9,6 +20,7 @@ val lines = file flatMap { line => {
 }
 
 // lines.saveAsTextFile("wordList.txt")
+// driver get the value
 println(s"Blank lines: ${blankLines.value}")
 
 
